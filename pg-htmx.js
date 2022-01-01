7879823f578260369442b7532950480282e3f684
Robert Means <robert@robertmeans.net>
2022-01-01 08:35:38 -0500

    Adds menu and script function

--- pg-htmx.js
+++ pg-htmx.js
@@ -1,21 +1,51 @@
 $(function () {
     $('body').one('pinegrow-ready', function (e, pinegrow) {
-      let type_prefix = 'pg-htmx';
-  
-      var framework = new PgFramework(type_prefix, 'HTMXforPG');
-  
-      framework.type = 'htmx';
-      framework.allow_single_type = true;
-  
-      framework.description = 'This framework adds support for the HTMX library to Pinegrow.';
-      framework.author = "'Bo' @ springhilldesign.net";
-      framework.author_link = 'https://plugins.springhilldesign.net';
-  
-      framework.has_actions = true;
-  
-      pinegrow.addFramework(framework);
-  
-      //Uncomment the line below to get the inspector window to automatically open
-      require('nw.gui').Window.get().showDevTools();
+        let type_prefix = 'pg-htmx';
+
+        var framework = new PgFramework(type_prefix, 'HTMXforPG');
+
+        framework.type = 'htmx';
+        framework.allow_single_type = true;
+
+        framework.description = 'This framework adds support for the HTMX library to Pinegrow.';
+        framework.author = "'Bo' @ springhilldesign.net";
+        framework.author_link = 'https://plugins.springhilldesign.net';
+
+        framework.has_actions = true;
+
+        pinegrow.addFramework(framework);
+
+        //Uncomment the line below to get the inspector window to automatically open
+        require('nw.gui').Window.get().showDevTools();
+
+        //Adds a menu item for adding the HTMX script
+        framework.on_page_menu = (page, items) => {
+            items.push({
+                type: 'divider'
+            });
+
+            items.push({
+                type: 'header',
+                label: 'HTMX'
+            });
+
+            items.push({
+                label: 'Add HTMX script',
+                action: function () {
+                    addHTMXScript();
+                }
+            });
+
+            items.push({
+                type: 'divider'
+            })
+        };
+
+        //Function to add HTMX script - ommited version so should load latest
+        let addHTMXScript = () => {
+            let thePage = pinegrow.getSelectedPage();
+            thePage.addScript('https://unpkg.com/htmx.org', true, false, false);
+        }
     });
+
 });
